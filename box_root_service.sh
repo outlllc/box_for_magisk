d0f5789caab6925692b47f18fe717edc8c11cb14
outlllc <outlocklike@outlook.com>
2025-12-09 23:25:44 +0800

    (ColorOS16): automatically detect all OnePlus devices and remove the device firewall blocking rules for Google services after a sixty second startup delay
    copy&paste from GitMetaio/Surfing

--- box_root_service.sh
+++ box_root_service.sh
@@ -12,3 +12,36 @@
         echo "File /data/adb/boxroot/scripts/start.sh not found" > "/data/adb/boxroot/run/box_service.log"
     fi
 ) &
+
+delete_op_coloros16_fw_rules() {
+    brand=$(getprop ro.product.brand | tr '[:upper:]' '[:lower:]')
+    case "$brand" in
+        oppo|oneplus|realme|oplus)
+            ;;
+        *)
+            return 0
+            ;;
+    esac
+    sleep 60
+    CHAINS="fw_INPUT fw_OUTPUT"
+    PROTOS="ipv4 ipv6"
+    for proto in $PROTOS; do
+        case "$proto" in
+            ipv4) cmd="iptables" ;;
+            ipv6) cmd="ip6tables" ;;
+        esac
+        
+        for chain in $CHAINS; do
+            $cmd -t filter -nL "$chain" >/dev/null 2>&1 || continue
+            lines=$($cmd -t filter -nL "$chain" --line-numbers \
+                    | grep "REJECT" \
+                    | awk '{print $1}' \
+                    | sort -rn)
+            for line in $lines; do
+                [ -n "$line" ] && [ "$line" -gt 0 ] || continue
+                $cmd -t filter -D "$chain" "$line" 2>/dev/null
+            done
+        done
+    done
+}
+delete_op_coloros16_fw_rules &
